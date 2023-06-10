ad7faef2b0023f60da9f4833775713aec1c9c9dc
hannah <hanna0115@naver.com>
2023-06-10 23:04:18 +0900

    ✨ sidebar 클릭시 color/transform 효과 넣고 스크롤 생성

--- 230609_LEGO(반응형)실습/js/sidebarDrawer.js
+++ 230609_LEGO(반응형)실습/js/sidebarDrawer.js
@@ -1,11 +1,14 @@
 document.querySelector('.drawer-menu1').addEventListener('click', function(){
+  document.querySelector('.menu1').classList.add('is-active');
   document.querySelector('.lego-shopping-list').classList.add('is-active');
 })
 
 document.querySelector('.drawer-menu2').addEventListener('click', function(){
+  document.querySelector('.menu2').classList.add('is-active');
   document.querySelector('.lego-brand-intro-list').classList.add('is-active');
 })
 
 document.querySelector('.drawer-menu3').addEventListener('click', function(){
+  document.querySelector('.menu3').classList.add('is-active');
   document.querySelector('.customer-support-list').classList.add('is-active');
 })
