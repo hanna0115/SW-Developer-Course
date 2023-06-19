8d235ccaeb9a8163fbad3b7ba6865f924baab306
hanna0115 <hanna0115@naver.com>
2023-06-19 16:41:29 +0900

    📝 설명 추가

--- 230619_variable/최저시급 계산기/script.js
+++ 230619_variable/최저시급 계산기/script.js
@@ -17,7 +17,10 @@ const actionElem = document.getElementById('action')
 actionElem.addEventListener('click', () => {
   // 근무시간
   const timeElem = document.getElementById('time')
-  let timeValue = timeElem.value
+  
+  // 숫자일 경우, +를 붙이면 string -> number 형 변환
+  // parseInt(value) string -> number 형 변환
+  let timeValue = +timeElem.value
   console.log(timeValue)
 
   //결과 계산 = 근무시간 x 최저시급
@@ -28,3 +31,24 @@ actionElem.addEventListener('click', () => {
 })
 // console.log(actionElem)
 // console.log(result)
+
+// ES6+, ESNEXT
+const func = (number, text) => console.log(number, text)
+
+// var : 예기치 못한 상황을 유발하기 때문에,
+// 숙련되지 못한 경우엔 오류가 발생할 수 있어
+// 점차 사용하지 않는 추세
+
+// let, const
+// let : 변할 수 있는 값, 내용
+// const : 변할 수 없는 것들.
+
+// console.log() : 내가 가져온 값을 실질적으로 제대로 불러오고 있는지 확인
+// console.log(typeof 변수명) // number, string, boolean 등 타입 체크
+// 타입 자료형 - 자료의 형태 - number, string, boolean, object ...
+
+// addEventListener : 이벤트를 청취하겠다. 관찰하겠다.
+// 사용자가 특정 동작을 했다.
+// addEventListener('click', function(){})
+// mousemove, scroll, resize ... mouseover, mouseleave, moustenter.
+// touch event
