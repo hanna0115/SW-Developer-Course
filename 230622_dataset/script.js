c09c003ae30cdca3198f59bfa5750d0714159bfe
hanna0115 <hanna0115@naver.com>
2023-06-22 15:59:40 +0900

    📝 그래프가 여러 개일 때

--- 230622_dataset/script.js
+++ 230622_dataset/script.js
@@ -1,11 +1,26 @@
-const bar = document.querySelector('.bar')
-const number = document.querySelector('.number')
+const container = document.querySelectorAll('.container')
 
-// data-number, dataset.number
-const dataNumber = bar.dataset.number;
-// number에 숫자 변경/삽입
-number.textContent = dataNumber;
-// bar 그래프의 width 값으로 퍼센트 표현
-bar.style.width = dataNumber + '%';
+container.forEach(item => {
+  // item => 부모, container
+  // container[0], container[1], container[2]
+  // 부모에서 bar 찾기
+  const bar = item.querySelector('.bar')
+  // 부모에서 number 찾기
+  const number = item.querySelector('.number')
 
-console.log(dataNumber)
+  // data-number값에 접근할 때 -> dataset.number
+  // bar.data-number -> 자바스크립트는 못 알아듣는다.
+  // bar.data.number -> 자바스크립트가 알아듣는다.
+  const dataNumber = bar.dataset.number;
+
+  // number에 숫자 변경/삽입
+  number.textContent = dataNumber;
+  
+  // bar 그래프의 width 값으로 퍼센트 표현
+  bar.style.width = dataNumber + '%';
+  // console.log(bar.dataset.number)
+})
+
+
+
+// console.log(dataNumber)
