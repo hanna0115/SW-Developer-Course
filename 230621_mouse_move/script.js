7ef0e3fb1a238d61fcf0981ab70362eb0a3ea762
hanna0115 <hanna0115@naver.com>
2023-06-22 12:56:26 +0900

    📝중앙정렬,clienstWidth/Height,offsetWidth/Height 추가설명

--- 230621_mouse_move/script.js
+++ 230621_mouse_move/script.js
@@ -8,10 +8,21 @@ window.addEventListener('mousemove', (event) => {
   // pointer.style.left = event.clientX + 'px';
   // pointer.style.top = `${event.clientY}px`;
   // pointer.style.left = `${event.clientX}px`;
-  // [pointer를 cursor 정중앙 정렬]
+
+    // [pointer를 cursor 정중앙 정렬]
+  // event객체에서 사용자의 x값을 가져와서, 
+  // pointer의 가로값(36px)을 반으로 나눈 18px을 뺀다.
+  // 예) x값이 100일 경우, 100 - 18, 82px, 커서는 위치가 82px,
+  // pointer.style.top = event.clientY - (pointer.offsetHeight / 2) + 'px'
   pointer.style.top = `${event.clientY - (pointer.offsetHeight / 2)}px`;
   pointer.style.left = `${event.clientX - (pointer.offsetWidth / 2)}px`;
 
+
+  // clientWidth,clientHeight의 경우에는 margin, border 사이즈를 무시한다
+  // clientWidth,clientHeight는 border(1px solid white) 포함 36px 사이즈를 인식하지 못하고,
+  // 좌우상하 1px씩 제외한 34px만 인식을 한다.
   // console.log(pointer.clientWidth)
+
+  // offsetWidth,offsetHeight는 border 사이즈도 포함해서 가져온다.
   // console.log(pointer.offsetWidth)
 })
