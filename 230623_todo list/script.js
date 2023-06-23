e5330cb2877a0ae08f422517971b9f9b6a553316
hanna0115 <hanna0115@naver.com>
2023-06-23 16:52:09 +0900

    📝 입력 후 추가, 완료, 삭제 가능한 todo list 완성(enter키로 추가 가능)

---
+++ 230623_todo list/script.js
@@ -0,0 +1,33 @@
+const input = document.getElementById('add-todo')
+const addBtn = document.getElementById('add-button')
+const list = document.getElementById('list')
+
+const addList = () => {
+  input.focus()
+  if(input.value.length === 0){
+    return alert('문자를 하나라도 입력해주세요')
+  }
+  const li = document.createElement('li')
+  li.innerHTML = `${input.value}
+  <button class="list-delete">&#x2716</button>`;
+  li.classList.add('list-item');
+  list.appendChild(li);
+  input.value = '';
+}
+
+input.addEventListener('keydown', (event) => {
+  if(event.key === 'Enter'){
+    addList()
+  }
+})
+
+addBtn.addEventListener('click', () => addList)
+
+list.addEventListener('click', (event) => {
+  if(event.target.tagName === 'LI'){
+    event.target.classList.toggle('checked');
+  }
+  if(event.target.tagName === 'BUTTON'){
+    event.target.parentElement.remove();
+  }
+})
