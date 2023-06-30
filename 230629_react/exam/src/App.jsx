7f5324126bd8f8da0daf8a12f6ab6da4d860401b
hanna0115 <hanna0115@naver.com>
2023-06-30 16:22:30 +0900

    ✨ image 컴포넌트

--- 230629_react/exam/src/App.jsx
+++ 230629_react/exam/src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
-import Button from './components/Button.jsx'
-import Heading from './components/Heading.jsx'
 import Flex from './components/Flex.jsx'
+import Image from './components/Image'
+
+const images = [
+  {id: 1, url:'/image1.jpg', title:'풍경1'},
+  {id: 2, url:'/image2.jpg', title:'풍경2'},
+  {id: 3, url:'/image3.jpg', title:'풍경3'}
+]
 
 function App() {
   return (
     <>
       <Flex>
-        <Heading title="안녕하세요"/>
-        <Button name="1번버튼" primary={true}/>
-        <Button name="2번버튼" secondary/>
-        <Button name="3번버튼" />
+        {/* key값은 부모 태그에 넣어주기 */}
+        {images.map(image => (
+          <div key={image.id}>
+            <Image url={image.url} title={image.title}/>
+            <h2>{image.title}</h2>
+          </div>
+        ))}
       </Flex>
     </>
   )
