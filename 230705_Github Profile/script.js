46da223bbfac96bc4436d9c520c53ea0051664f0
hanna0115 <hanna0115@naver.com>
2023-07-06 13:09:17 +0900

    📝 코드 수정

--- 230705_Github Profile/script.js
+++ 230705_Github Profile/script.js
@@ -1,6 +1,14 @@
-const profileImg = $('.profile-img');
-const dropdownMenu = $('.dropdown-menu');
+// const profileImg = $('.profile-img');
+// const dropdownMenu = $('.dropdown-menu');
 
-profileImg.on('click', () => {
-  dropdownMenu.toggleClass('is-open');
-});
+// profileImg.on('click', () => {
+//   dropdownMenu.toggleClass('is-open');
+// });
+
+
+const profileImg = document.querySelector('.profile-img');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+profileImg.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('is-open')
+})
