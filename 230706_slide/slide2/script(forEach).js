9a48b6d62b34e339b6608dc613a34efcdf6b45ca
hanna0115 <hanna0115@naver.com>
2023-07-10 13:36:02 +0900

    ✨ 조건문으로 button active 클래스 추가/제거

--- 230706_slide/slide2/script(forEach).js
+++ 230706_slide/slide2/script(forEach).js
@@ -3,8 +3,21 @@ const slideList = document.querySelector('.slide-list');
 
 buttons.forEach(function(btn, index){
   btn.addEventListener('click', () => {
-    buttons.forEach(item => {item.classList.remove('active')})
     slideList.style.transform = `translateX(-${index}00vw)`;
+
+    // // buttons라는 배열을 한 번 더 돌면서 모든 버튼이 가지고 있는 active class remove
+    // buttons.forEach(otherBtn => {
+    //   otherBtn.classList.remove('active');
+    // })
+    // 클릭된 버튼에만 active class add
     btn.classList.add('active');
+
+    // 조건문
+    // 현재 클릭되어 있는 버튼이 아닐 때 && active라는 class를 포함하고 있다면 조건이 true일 때 active class remove
+    buttons.forEach(function(otherBtn){
+      if(otherBtn !== btn && otherBtn.classList.contains('active')){
+        otherBtn.classList.remove('active');
+      }
+    })
   })
 })
