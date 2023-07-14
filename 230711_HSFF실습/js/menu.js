12e535618a5cc15dd4a711476e023ff00e9c9102
hanna0115 <hanna0115@naver.com>
2023-07-14 13:42:51 +0900

    ✨ is-active class add / remove

--- 230711_HSFF실습/js/menu.js
+++ 230711_HSFF실습/js/menu.js
@@ -16,6 +16,17 @@ gnbItems.forEach(function(item){
   let subMenu = item.querySelector('.sub');
   
   item.addEventListener('mouseover', function(){
+    // sub 메뉴가 보이지 않게 max-height를: 265px
     subMenu.classList.add('active');
+    // gnb-main-item a 태그에 on class 추가시 메인메뉴 바뀐 컬러 유지
+    this.querySelector('a').classList.add('on');
+    this.querySelector('span').classList.add('is-active');
+  })
+
+  item.addEventListener('mouseleave', function(){
+    // sub 메뉴가 보이지 않게 max-height를: 0
+    subMenu.classList.remove('active');
+    this.querySelector('a').classList.remove('on');
+    this.querySelector('span').classList.remove('is-active');
   })
 })
