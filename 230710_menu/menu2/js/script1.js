68396e89ebdbbe824403100e8a23c1e4aeb56bab
hanna0115 <hanna0115@naver.com>
2023-07-17 13:34:05 +0900

    ✨ .submenu-wrapper slideUp& slideDown

---
+++ 230710_menu/menu2/js/script1.js
@@ -0,0 +1,12 @@
+// find(): 자식 및 하위 태그 찾을 때
+// children(): 자식 요소만 찾을 때
+
+// index , eq()
+
+$('.main-menu li, .submenu-wrapper').hover(function(){
+  // hover시 실행할 함수
+  $('.submenu-wrapper').stop().slideDown(500);
+}, function(){
+  // leave 됐을 때 실행할 함수
+  $('.submenu-wrapper').stop().slideUp(500);
+})
