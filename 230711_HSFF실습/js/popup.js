984231185837142caf4de0f48220524a45e3a11f
hanna0115 <hanna0115@naver.com>
2023-07-17 16:39:38 +0900

    ✨ popup창 구현

---
+++ 230711_HSFF실습/js/popup.js
@@ -0,0 +1,6 @@
+const xBtn = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal');
+
+xBtn.addEventListener('click', function(){
+  modal.style.display = 'none';
+})
