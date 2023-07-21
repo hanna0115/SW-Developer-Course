96a424ecc4162a6c0a56bc80190a026b6585a688
hanna0115 <hanna0115@naver.com>
2023-07-21 13:47:46 +0900

    ✨ dots 안에 반복문으로 slide-item 길이만큼 div 생성

--- 230706_slide/slide4(setInterval)/js/script.js
+++ 230706_slide/slide4(setInterval)/js/script.js
@@ -1,43 +1,8 @@
-const circleBtn = document.querySelectorAll('.circle-button-list button');
-const slideWrapper = document.querySelector('.slide-wrapper');
+// 현재 인덱스를 저장하는 변수 (초기값 0)
+let cur = 0;
+// len이라는 변수에 .slide-item라는 클래스가 가진 요소들의 총 개수 저장
+let len = $('.slide-item').length;
 
-circleBtn.forEach(function(item, index){
-  item.addEventListener('click', function(){
-    circleBtn.forEach(function(item){
-      item.classList.remove('is-active')
-    })
-    slideWrapper.style.transform = `translateX(-${index}00vw)`;
-    item.classList.add('is-active')
-  })
-})
-
-let slideItems = document.querySelectorAll('.slide-list');
-
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-
-let crrentSlide = 0;
-
-// nextBtn.addEventListener('click', function(){
-//   if(crrentSlide < slideItems.length -1){
-//     crrentSlide++;
-//     slideWrapper.style.transform = `translateX(-${crrentSlide * 100}vw)`
-//   }
-// })
-
-// prevBtn.addEventListener('click', function(){
-//   if(crrentSlide > 0){
-//     crrentSlide--;
-//     slideWrapper.style.transform = 'translateX(-' + crrentSlide * 100 + 'vw)'
-//   }
-// })
-
-setInterval(function(){
-  if(crrentSlide < slideItems.length -1){
-    crrentSlide++;
-    slideWrapper.style.transform = `translateX(-${crrentSlide * 100}vw)`
-  }else{
-    crrentSlide = 0;
-    slideWrapper.style.transform = `translateX(-${crrentSlide * 100}vw)`
-  }
-}, 3000)
+for(i = 0; i < len; i++) {
+  $('#dots').append('<div></div>');
+}
