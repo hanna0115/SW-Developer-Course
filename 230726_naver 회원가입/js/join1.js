24de8ea431a5a3a91f94dc2a755a40cf45a77eb3
hanna0115 <hanna0115@naver.com>
2023-08-07 13:50:39 +0900

    ✨ Vanilla JS로 비밀번호 조건문 구현

--- 230726_naver 회원가입/js/join1.js
+++ 230726_naver 회원가입/js/join1.js
@@ -14,6 +14,17 @@ let mailveri = true;
 // Essential Infomation
 let essenInfo = '<span class="text-red">필수 정보입니다.</span>';
 
+// // .warn red text
+// function red(user, text){
+//   document.querySelector(`.${user} .warn`).innerHTML = '<span class="text-red">' + text + '</span>'
+// }
+
+// // .warn green text
+// function green(user, text){
+//   document.querySelector(`.${user} .warn`).innerHTML = '<span class="text-green">' + text + '</span>'
+// }
+
+
 // 아이디
 document.querySelector('.userid input').addEventListener('focusout', function(){
   let userId = this.value;
@@ -29,5 +40,31 @@ document.querySelector('.userid input').addEventListener('focusout', function(){
     // 위 조건에 둘 다 해당하지 않고 값을 잘 입력했을 때
     idveri = true;
     idWarn.innerHTML = '<span class="text-green">멋진 아이디네요!</span>';
+  }
+})
+
+// 비밀번호
+document.querySelector('.userpw input').addEventListener('focusout', function(){
+  let userPw = this.value;
+  let pwExp = /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,20}$/
+
+  let pwWarn = document.querySelector('.userpw .warn');
+  let pwText = document.querySelector('.userpw .inputbox p');
+  let pwImg = document.querySelector('.userpw .inputbox img');
+
+  if(userPw.length == 0) {
+    pwWarn.innerHTML = essenInfo;
+    // pwText 내용 비우기
+    pwText.innerHTML = '';
+    pwImg.src = './images/m_icon_pw_step_01.png'
+  } else if(!pwExp.test(userPw)) {
+    pwWarn.innerHTML = '<span class="text-red">8~20자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</span>';
+    pwText.innerHTML = '<span class="text-red">사용불가</span>'
+    pwImg.src = './images/m_icon_pw_step_10.png'
+  } else {
+    pwveri = true;
+    pwWarn.innerHTML = '';
+    pwText.innerHTML = '<span class="text-green">안전</span>'
+    pwImg.src = './images/m_icon_pw_step_04.png'
   }
 })
