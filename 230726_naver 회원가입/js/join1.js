928e747b67977ee3a3c83ec8b3a1a454684f15f9
hanna0115 <hanna0115@naver.com>
2023-08-08 13:30:36 +0900

    ✨ join 버튼 클릭시 필수사항이 모두 true라면 제출, 아니면 preventDefault

--- 230726_naver 회원가입/js/join1.js
+++ 230726_naver 회원가입/js/join1.js
@@ -291,3 +291,18 @@ function sample6_execDaumPostcode() {
       }
   }).open();
 }
+
+
+// 가입하기 버튼 제출
+document.getElementById('joinbtn').addEventListener('click', function(e){
+  let isTrue = idveri && pwveri && pwchkveri && nameveri && birthveri && genderveri && mailveri && phoneveri && addressveri;
+
+  if(isTrue) {
+    document.getElementById('join-form').submit();
+  } else {
+    e.preventDefault();
+    document.querySelectorAll('input').forEach(function(input){
+      input.dispatchEvent(new Event('focusout'));
+    })
+  }
+})
