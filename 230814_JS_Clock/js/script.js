b45addb7e8dd97cb110f7c3163f8afaf99d5fa63
hanna0115 <hanna0115@naver.com>
2023-08-18 14:38:09 +0900

    ✨ 시간에 맞춰서 시간/배경 이미지 변경 및 버튼 활성화, 버튼 클릭시 배경 이미지 변경 및 버튼 활성화

--- 230814_JS_Clock/js/script.js
+++ 230814_JS_Clock/js/script.js
@@ -1,5 +1,8 @@
+let time = new Date();
+let hours = time.getHours();
+
 // .phone-box span 1초마다 시간 바뀌기
-function nowTime(){
+setInterval(function(){
   let time = new Date();
   let hours = time.getHours();
   let minutes = time.getMinutes();
@@ -19,63 +22,45 @@ function nowTime(){
   seconds = addZero(seconds);
 
   $('.phone-box span').text(`${hours} : ${minutes} : ${seconds}`);
+}, 1000)
+
+
+// 시간에 맞춰서 배경 이미지, 버튼 활성화
+let btnList = document.querySelectorAll('.btn-box button');
+let back = document.querySelector('.wrapper');
+
+if(hours >= 6 && hours < 12) {
+  back.classList.add('morning');
+  btnList[0].classList.add('on');
+} else if(hours >= 12 && hours < 16) {
+  back.classList.add('afternoon');
+  btnList[1].classList.add('on');
+} else if(hours >= 16 && hours < 20) {
+  back.classList.add('evening');
+  btnList[2].classList.add('on');
+} else {
+  back.classList.add('night');
+  btnList[3].classList.add('on');
 }
 
-setInterval(nowTime, 1000)
 
-function nowPart(){
-  let time = new Date();
-  let hours = time.getHours();
-
-  if(hours > 6 && hours < 11) {
-
-  }
-}
-
-
-
-function partOfDay(part){
-  document.querySelector('.wrapper').style.backgroundImage = "url('./images/bg_"+part+".jpg')";
-  document.querySelector('.phone-img').src = './images/phone_'+part+'.png';
-  document.querySelector('.phone-img').alt = part
-}
-
-document.querySelectorAll('.btn-box button').forEach(function(button){
-  button.addEventListener('click', function(){
-    document.querySelectorAll('.btn-box button').forEach(function(button){
-      button.classList.remove('on')
+// 버튼 클릭시 배경 이미지 변경, on class add & remove
+btnList.forEach(function(item){
+  let backTxt = item.innerHTML;
+  
+  item.addEventListener('click', function(e){
+    btnList.forEach(function(item){
+      item.classList.remove('on')
+      back.className = 'wrapper';
     })
-    button.classList.add('on')
+
+    item.classList.add('on');
+    back.classList.add(backTxt);
+
+    if(backTxt == 'night') {
+      document.querySelector('.logo img').src = './images/logo_w.png';
+    } else {
+      document.querySelector('.logo img').src = './images/logo_b.png';
+    }
   })
-
-  if(this.classList.contains('afternoon')){
-    partOfDay(afternoon);
-  }
 })
-
-// $('.btn-box button').on('click', function(){
-//   $('.btn-box button').removeClass('on');
-//   $(this).addClass('on')
-
-//   if($(this).hasClass('morning')){
-//     $('.wrapper').css('background-image', "url('./images/bg_morning.jpg')");
-//     $('.phone-img').attr('src', './images/phone_morning.png');
-//     $('.phone-img').attr('alt', 'morning');
-//   } else if($(this).hasClass('afternoon')){
-//     // $('.wrapper').css('background-image', "url('./images/bg_afternoon.jpg')");
-//     // $('.phone-img').attr('src', './images/phone_afternoon.png');
-//     // $('.phone-img').attr('alt', 'afternoon');
-//     partOfDay(afternoon)
-//   } else if($(this).hasClass('evening')){
-//     $('.wrapper').css('background-image', "url('./images/bg_evening.jpg')");
-//     $('.phone-img').attr('src', './images/phone_evening.png');
-//     $('.phone-img').attr('alt', 'evening');
-//   } else if($(this).hasClass('night')){
-//     $('.wrapper').css('background-image', "url('./images/bg_night.jpg')");
-//     $('.phone-img').attr('src', './images/phone_night.png');
-//     $('.phone-img').attr('alt', 'night');
-//     $('.logo img').attr('src','./images/logo_w.png');
-//     $('.main-txt p').css('color', '#666');
-//     $('.gnb-item a, .main-txt span, phone-box span').css('color', '#fff');
-//   }
-// })
