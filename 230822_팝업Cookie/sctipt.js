b64f77c3ddbddabc6de3df8e65205a92a81def84
hanna0115 <hanna0115@naver.com>
2023-08-22 15:17:20 +0900

    ✨ 쿠키 만료시간 date.toUTCString( )으로 변환

--- 230822_팝업Cookie/sctipt.js
+++ 230822_팝업Cookie/sctipt.js
@@ -25,7 +25,21 @@ function setCookie(name, value, hours){
   if(hours) {
     // date라는 변수 안에 현재 시간을 가지는 Date 객체를 생성
     let date = new Date();
+
+    // date 값을 현재 시간 + hours(ms) 더한 값으로 set 한다.
+    // 쿠키 만료시간이 hours 더한 시간만큼 뒤로 설정된다.
+
+    // 시간을 밀리초(ms)로 변환
     // hour(시간) * 60(분) * 60(초) * 1000(밀리초)
+    // javasctipt에서 시간을 다룰 때 일반적으로 밀리초(ms)로 계산한다.
+    // Date 객체가 밀리초(ms) 단위로 시간을 나타내기 때문
     date.setTime(date.getTime() + (hours * 60 * 60 * 1000))
+
+    // ;(세미콜론)
+    // 쿠키 설정은 문자열 네임과 벨류로 구성되는데,
+    // 각각 이름과 값을 세미콜론으로 구분한다.
+    // toUTCString() : Date 객체의 시간 => UTC 형태의 문자열로 변환
+    // 쿠키 만료시간을 표준화된 방식으로 변환
+    expires = '; expires' + date.toUTCString();
   }
 }
