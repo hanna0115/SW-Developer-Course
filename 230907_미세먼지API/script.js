c42a6a3a450aed240567926353e25e41f03ecfe0
hanna0115 <hanna0115@naver.com>
2023-09-07 13:47:25 +0900

    👽️ 공공데이터포털 미세먼지 API

---
+++ 230907_미세먼지API/script.js
@@ -0,0 +1,19 @@
+var xhr = new XMLHttpRequest();
+var url = 'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getMsrstnAcctoRDyrg'; /*URL*/
+var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'K3ffxC1oIoWzYskEUMHmA3hfplXmJTt08QidPS9Br4fcnakaukocNyaP5ADWFtSMQUivJzOwjmKlnqVUEADYXQ%3D%3D'; /*Service Key*/
+queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /* 응답 데이터 타입 설정 */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* 한 페이지 결과 수 */
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지 번호 설정 */
+queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent('대전'); /* 조회할 데이터 시도 이름 설정 */
+queryParams += '&' + encodeURIComponent('searchCondition') + '=' + encodeURIComponent('DAILY'); /* 데이터 기간 */
+xhr.open('GET', url + queryParams);
+xhr.onreadystatechange = function () {
+  if (this.readyState == 4) {
+    if(this.status === 200) {
+      let responsData = JSON.parse(this.responseText)
+      console.log(responsData)
+    }
+  }
+};
+
+xhr.send('');
