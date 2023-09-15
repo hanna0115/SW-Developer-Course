98eaef115d5a523381af8c07aeaaf7fd94213b18
hanna0115 <hanna0115@naver.com>
2023-09-15 16:46:48 +0900

    ✨ Destructuring

---
+++ React/01.JavascriptES6/02.js
@@ -0,0 +1,25 @@
+// Destructuring
+// 구조 분해 할당
+// 구조를 분해해서 값을 가져온다.
+let person = {
+  name : 'hanna',
+  age : 23
+}
+
+//  console.log(person.name)
+//  console.log(person.age)
+
+// 단축 표기법
+// 먼저 변수 선언 후,
+// 객체를 쓰고 그 안에 내가 가져오고 싶은 key값 입력
+// person이라는 객체에서 name과 age라는 key값을
+// 분해한 값을 let으로 선언한 변수에 할당
+let {name, age1} = person;
+console.log(name)
+
+// 해당 문법을 Array에도 적용 가능
+// num1, num2, num3이라는 변수가 생성되고, 그 변수 안에는
+// 배열의 순서대로 num1에는 arr배열의 0번째 값이 할당된다.
+let arr = [1, 2, 3, 4, 5]
+let [num1, num2, num3] = arr;
+console.log(num1, num2, num3)
