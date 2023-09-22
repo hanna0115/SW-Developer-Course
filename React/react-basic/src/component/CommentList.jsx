35bb00360fda7ec605a41f9e2f3ce7d178f811fc
hanna0115 <hanna0115@naver.com>
2023-09-22 16:19:16 +0900

    ✨ 글작성 기능 구현 완료

--- React/react-basic/src/component/CommentList.jsx
+++ React/react-basic/src/component/CommentList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Comment from './Comment';
 
 let comments = [
@@ -17,15 +17,38 @@ let comments = [
 ]
 
 const CommentList = () => {
+  let [list, setList] = useState(comments);
+  let [name, setName] = useState('');
+  let [text, setText] = useState('');
+
+  // 삼항연산자로 value 값이 빈 값이라면
+  // list가 추가되지 않고 alert 창 띄우기
+  let addComment = () => {
+    name === '' || text === '' ? alert('이름과 내용을 입력하세요.') : (
+      // 즉시 실행 함수 (IIFE)
+      // 삼항연산자 조건이 false일 때 -> name, text에 값이 있을 때
+      // 다음 함수가 즉시 실행되도록 한다.
+      (() => {
+        let add = {name : name, content : text};
+        setList([add, ...list]);
+        setName('');
+        setText('');
+      })()
+    )
+  }
+
   return (
     <div>
       {
-        comments.map((item, index) => {
+        list.map((item, index) => {
           return (
             <Comment name={item.name} text={item.content} key={index}/>
           )
         }) 
       }
+      <span>작성자 : <input value={name} onChange={(e) => setName(e.target.value)}/></span>
+      <span>내용 : <input value={text} onChange={(e) => setText(e.target.value)}/></span>
+      <button type='button' onClick={addComment}>글 작성</button>
     </div>
   );
 };
