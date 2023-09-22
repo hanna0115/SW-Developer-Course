cde421a5d74268c63d3c28d825e9dc8f62159865
hanna0115 <hanna0115@naver.com>
2023-09-22 14:41:07 +0900

    💄 Comment component styling

---
+++ React/react-basic/src/component/Comment.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+let styles = {
+  wrapper : {
+    display : 'flex',
+    justifyContent : 'space-between',
+    alignItems : 'center',
+    padding : '0 20px',
+    border : '1px solid #ccc',
+    borderRadius : '16px'
+  },
+
+  img : {
+    width : '50px',
+    height : '50px',
+    borderRadius : '50%'
+  },
+
+  contentBox : {
+    flexGrow : 1,
+    marginLeft : '16px'
+  }
+}
+
+const Comment = () => {
+  return (
+    <div style={styles.wrapper}>
+      <div>
+        <img style={styles.img} src="https://cdn.icon-icons.com/icons2/1161/PNG/512/1487716857-user_81635.png" alt="" />
+      </div>
+
+      <div style={styles.contentBox}>
+        <h3>User name</h3>
+        <p>Content</p>
+      </div>
+
+      <button type='button'>X</button>
+    </div>
+  );
+};
+
+export default Comment;
