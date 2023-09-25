8fa9de16fbbfdb32329c9b37d60bf1cfa24e6b6a
hanna0115 <hanna0115@naver.com>
2023-09-25 14:16:09 +0900

    🎉 리액트 환경구축 완료

---
+++ React/rock/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Box from './component/Box';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='box-list'>
+        <Box title='You'/>
+        <Box title='Computer'/>
+      </div>
+      <div className="btn-list">
+        <button type='button'>가위</button>
+        <button type='button'>바위</button>
+        <button type='button'>보</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
