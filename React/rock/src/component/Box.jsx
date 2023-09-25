5593f25f9214eed8708fde09d2bca81244746b59
hanna0115 <hanna0115@naver.com>
2023-09-25 14:38:29 +0900

    🚧 유저 선택, 컴퓨터 선택, 결과 state

--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './Box.css'
 
 const Box = (props) => {
   return (
-    <div>
+    <div className='box-item'>
       <h1>{props.title}</h1>
       <h2>Rock</h2>
       <img src="https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day.jpg" alt="바위 이미지" />
